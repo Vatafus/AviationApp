4e26373a8f9d471c258a7d56d3d4ef5677d4ab6f
SirRoxer <vtstiv@gmail.com>
2022-11-22 21:50:43 +0200

    RegisterUpdate
    - Am lucrat pe partea estetica a paginii de registrare.

--- aviation/src/app/register/register.component.ts
+++ aviation/src/app/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../class/user';
+import { Router } from '@angular/router';
+import { RegisterService } from '../services/register.service';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  user: User = new User();
+
+  constructor(private router: Router, private registerService: RegisterService) { }
 
   ngOnInit(): void {
   }
+
+  userRegister() {
+    console.log(this.user);
+    this.registerService.registerUser(this.user).subscribe(data => {
+      alert("Successfully User register")
+    });
+
+  }
+
 
 }
--- aviation/src/app/services/register.service.ts
+++ aviation/src/app/services/register.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../class/user';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RegisterService {
+  baseUrl = "http://localhost:8080/user/register"
 
-  constructor() { }
+  constructor(private httpclient: HttpClient) { }
+
+  registerUser(user: User): Observable<Object> {
+    console.log(user);
+    return this.httpclient.post(`${this.baseUrl}`, user);
+  }
 }
