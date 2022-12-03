ac16880aaadf38d4cb4704c95396664e6428aa8c
SirRoxer <vtstiv@gmail.com>
2022-12-03 14:59:46 +0200

    Added Role Entity

---
+++ BackEnd/Aviation/src/main/java/com/example/aviation/domain/Role.java
@@ -0,0 +1,18 @@
+package com.example.aviation.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "role")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+}
