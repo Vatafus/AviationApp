f98e06634d0562d4c1056193b5e32b615b70fc4b
SirRoxer <vtstiv@gmail.com>
2023-01-07 19:17:49 +0200

    FrontEnd
    -Adaugarea comentariilor pe servicii.

--- FrontEnd/aviation/src/app/services/route-guard.service.ts
+++ FrontEnd/aviation/src/app/services/route-guard.service.ts
@@ -10,6 +10,9 @@ export class RouteGuardService implements CanActivate {
 
   constructor(private loginService: LoginService) { }
 
+  //Se foloseste conceptul de route-guard pentru autorizarea accesului in aplicatie.
+  //Am folosit functia CanActivate pentru a restrictiona accesul utilizatorilor care nu sunt logati,
+  //in diferite parti ale aplicatiei, verificand astfel daca userul este logat prin Injectarea Serviciului de logare.
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
     if (this.loginService.isUserLoggedIn())
       return true;
