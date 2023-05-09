eba387d76ecc807c0e359a493dece8f6e1c105c7
SirRoxer <vtstiv@gmail.com>
2023-05-09 18:14:07 +0300

    adding Role logic

--- BackEnd/Aviation/src/main/java/com/example/aviation/domain/User.java
+++ BackEnd/Aviation/src/main/java/com/example/aviation/domain/User.java
@@ -10,6 +10,8 @@ import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+import java.util.Set;
+
 
 @Data
 @Entity
@@ -28,11 +30,62 @@ public class User {
     @Column(name="cpassword")
     private String cpassword;
 
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name="USER_ROLE",
+    joinColumns = {
+            @JoinColumn(name = "USER_ID")
+    },
+            inverseJoinColumns = {
+            @JoinColumn(name = "ROLE_ID")
+            }
+    )
+    private Set<Role> role;
+
     @EqualsAndHashCode.Exclude
     @ManyToOne
     @JoinColumn(name = "user_id")
     private Booking booking;
 
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getCpassword() {
+        return cpassword;
+    }
+
+    public void setCpassword(String cpassword) {
+        this.cpassword = cpassword;
+    }
+
+    public Set<Role> getRole() {
+        return role;
+    }
+
+    public void setRole(Set<Role> role) {
+        this.role = role;
+    }
 
     public User(UserDTO user) throws UserRegistrationException {
         this.email = user.getEmail();
--- BackEnd/Aviation/src/main/java/com/example/aviation/service/UserService.java
+++ BackEnd/Aviation/src/main/java/com/example/aviation/service/UserService.java
@@ -1,17 +1,21 @@
 package com.example.aviation.service;
 
+import com.example.aviation.domain.Role;
 import com.example.aviation.domain.User;
 import com.example.aviation.dto.LoginDTO;
 import com.example.aviation.dto.UserDTO;
 import com.example.aviation.exception.InvalidPasswordException;
 import com.example.aviation.exception.NotLoggedInException;
 import com.example.aviation.exception.UserRegistrationException;
+import com.example.aviation.repo.RoleRepo;
 import com.example.aviation.repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Service
 public class UserService {
@@ -21,6 +25,37 @@ public class UserService {
     @Autowired
     public UserService(UserRepo userRepo) {
         this.userRepo = userRepo;
+    }
+
+    @Autowired
+    public RoleRepo roleRepo;
+
+    public void initRoleAndUser(){
+        Role adminRole = new Role();
+        adminRole.setRoleName("Admin");
+        adminRole.setRoleDescription("Admin user");
+        roleRepo.save(adminRole);
+
+        Role userRole = new Role();
+        userRole.setRoleName("User");
+        userRole.setRoleDescription("User user");
+        roleRepo.save(userRole);
+
+        User adminUser = new User();
+        adminUser.setEmail("admin@flybird.com");
+        adminUser.setPassword("admin");
+        adminUser.setCpassword("admin");
+        Set<Role> adminRoles = new HashSet<>();
+        adminRoles.add(adminRole);
+        userRepo.save(adminUser);
+
+        User user = new User();
+        user.setEmail("user@flybird.com");
+        user.setPassword("12345");
+        adminUser.setCpassword("12345");
+        Set<Role> userRoles = new HashSet<>();
+        userRoles.add(userRole);
+        userRepo.save(user);
     }
 
     public boolean isThereAlreadySuchEmail(String email) {
